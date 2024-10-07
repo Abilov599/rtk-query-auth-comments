d3d71d78b6d26d8b534da0ea902caf47b3e6437e
Abilov599 <abilovv599@gmail.com>
2024-10-07 18:16:43 +0400

    fix

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,17 +1,12 @@
-import { useNavigate } from 'react-router-dom';
-import { resetStore, selectCurrentUser } from '@/redux/auth-slice';
-import { useAppDispatch, useTypedSelector } from './store';
+import { selectCurrentUser } from '@/redux/auth-slice';
+import { useTypedSelector } from './store';
 
 export const useAuth = () => {
   const { accessToken, user } = useTypedSelector(selectCurrentUser);
   const isAuthenticated = Boolean(accessToken);
-  const dispatch = useAppDispatch();
-  const navigate = useNavigate();
 
   function logout() {
-    dispatch(resetStore());
     localStorage.removeItem('accessToken');
-    navigate('/login');
     window.location.reload();
   }
 
